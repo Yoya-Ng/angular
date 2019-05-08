f6231de1ec34c795102ac851b1fa6ad518357131
Yoya <unrealboundary@gmail.com>
2019-05-08 11:03:51 +0800

    H Service 呼叫 M Service 方法,將資料倒入M Service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,9 @@ export class HeroService {
   // }
   
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  
-  constructor() { }
+
+  constructor(private messageService: MessageService) { }
 }
